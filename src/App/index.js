e2941e69481cc6e58c364bc8d7d9735f65667d11
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2022-03-16 19:30:41 -0700

    upgrading yarn

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import GithubRepoCardSet from "../lib/components/react-gh-repo-cardset";
 
+// https://trekinbami.medium.com/using-environment-variables-in-react-6b0a99d83cf5
 var username = `${process.env.REACT_APP_GIT_USERNAME}`;
 var token = `${process.env.REACT_APP_GIT_TOKEN}`;
 
